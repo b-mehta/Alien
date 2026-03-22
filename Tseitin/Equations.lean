22feab558c3dce1296131b12123a5d0d36a63172
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-03-22 14:50:50 +0000

    add more examples

--- Tseitin/Equations.lean
+++ Tseitin/Equations.lean
@@ -16,16 +16,17 @@ lemma ea_swap' : x X a A = x A X := by simp only [← Tseitin.mul_assoc x, ea_sw
 lemma eb_swap' : x X b B = x B X := by simp only [← Tseitin.mul_assoc x, eb_swap]
 lemma acce' : x a A A X = x a A A := by simp only [← Tseitin.mul_assoc x, acce]
 
-macro "comm" : tactic =>
-  `(tactic| simp only [ac_comm, ad_comm, bc_comm, bd_comm, ac_comm', ad_comm', bc_comm', bd_comm'])
+macro "comm" : tactic => `(tactic|
+  simp only [ac_comm, ad_comm, bc_comm, bd_comm,
+             ac_comm', ad_comm', bc_comm', bd_comm', mul_assoc])
 
 macro "create" : tactic => `(tactic| simp only [acce, acce'])
 macro "move_a" : tactic => `(tactic| simp only [ea_swap, ea_swap'])
 macro "move_b" : tactic => `(tactic| simp only [eb_swap, eb_swap'])
 macro "move" : tactic =>   `(tactic| simp only [ea_swap, ea_swap', eb_swap, eb_swap'])
 
--- flipped from the website's problem 1
-lemma problem1 : a a A A A = a A A A := calc
+/-- This is a useful lemma because it lets us create or destroy `a` at the top. -/
+lemma duplicate : a a A A A = a A A A := calc
   _ = a A A a A   := by comm
   _ = a A A X a A := by create
   _ = a A A A X   := by move_a
@@ -33,81 +34,153 @@ lemma problem1 : a a A A A = a A A A := calc
   _ = A a A A     := by create
   _ = a A A A     := by comm
 
-private lemma problem1' : x a a A A A = x a A A A := by simp only [← Tseitin.mul_assoc x, problem1]
+private lemma duplicate' : x a a A A A = x a A A A := by
+  simp only [← Tseitin.mul_assoc x, duplicate]
 
-macro "problem1" : tactic => `(tactic| simp only [problem1, problem1'])
+macro "duplicate" : tactic => `(tactic| simp only [duplicate, duplicate'])
 
-lemma problem3 : a a A A A = a A A A := calc
-  _ = a A A a A   := by comm
-  _ = a A A X a A := by create
-  _ = a A A A X   := by move_a
-  _ = A a A A X   := by comm
-  _ = A a A A     := by create
-  _ = a A A A     := by comm
+namespace beginner
 
-private lemma problem3' : x a a A A A = x a A A A := by simp only [← Tseitin.mul_assoc x, problem3]
+lemma problem1 : a A A A = a a A A A := by duplicate
 
-macro "problem3" : tactic => `(tactic| simp only [problem3, problem3'])
-
-lemma problem2 : A A A X a = A A A X := calc
-  _ = A A X a A a       := by move_a
-  _ = A X a A a A a     := by move_a
-  _ = X a A a A a A a   := by move_a
+lemma problem2 : A A A X a = A A A X := by calc
+  _ = A A X a A a       := by move
+  _ = A X a A a A a     := by move
+  _ = X a A a A a A a   := by move
   _ = X a a a a A A A   := by comm
-  _ = X a a a A A A     := by problem3
+  _ = X a a a A A A     := by duplicate
   _ = X a A a A a A     := by comm
-  _ = A X a A a A       := by move_a
-  _ = A A X a A         := by move_a
-  _ = A A A X           := by move_a
+  _ = A X a A a A       := by move
+  _ = A A X a A         := by move
+  _ = A A A X           := by move
 
-private lemma problem2' : x A A A X a = x A A A X := by simp only [← Tseitin.mul_assoc x, problem2]
+-- We can combine multiple `move` steps:
+lemma problem2_alt : A A A X a = A A A X := by calc
+  _ = X a A a A a A a   := by move
+  _ = X a a a a A A A   := by comm
+  _ = X a a a A A A     := by duplicate
+  _ = X a A a A a A     := by comm
+  _ = A A A X           := by move
 
-macro "problem2" : tactic => `(tactic| simp only [problem2, problem2'])
+lemma problem3 : a a A A A = a A A A := by duplicate
 
-lemma problem4 : a b a a A A B A A = a A A B A A := calc
+lemma problem4 : a A A B A A = a b a a A A B A A := by calc
+  _ = A A B a A A         := by comm
+  _ = A A B a A A X       := by create
+  _ = a A A B A A X       := by comm
+  _ = a A A X b B a A a A := by move
+  _ = a A A b B a A a A   := by create
+  _ = a b a a A A B A A   := by comm
+
+lemma problem5 : A A X A = A A A X := by calc
+  _ = X a A a A A   := by move
+  _ = X a a A A A   := by comm
+  _ = X a a a A A A := by duplicate
+  _ = X a A a A a A := by comm
+  _ = A A A X       := by move
+
+lemma problem6 : a b a a A A B A A = a A A B A A := by calc
   _ = a A A b B a A a A   := by comm
   _ = a A A X b B a A a A := by create
-  _ = a A A B X a A a A   := by move
-  _ = a A A B A X a A     := by move
   _ = a A A B A A X       := by move
   _ = A A B a A A X       := by comm
   _ = A A B a A A         := by create
   _ = a A A B A A         := by comm
 
-private lemma problem4' : x a b a a A A B A A = x a A A B A A := by
-  simp only [← Tseitin.mul_assoc x, problem4]
+lemma problem7 : a b a A A A B A A A = a b a b a A A A B A A A := by calc
+  _ = A A A B a b a A A A         := by comm
+  _ = A A A B a b a A A X A       := by create
+  _ = A a b a A A B A A X A       := by comm
+  _ = A a b a A A X b B a A a A A := by move
+  _ = A a b a A A b B a A a A A   := by create
+  _ = a b a b A A A B a a A A A   := by comm
+  _ = a b a b A A A B a A A A     := by duplicate
+  _ = a b a b a A A A B A A A     := by comm
 
-macro "problem4" : tactic => `(tactic| simp only [problem4, problem4'])
+lemma problem8 : X A X b a A B = X A X A B X a := by calc
+  _ = X X a A b a A B   := by move
+  _ = X X a A A b B a   := by comm
+  _ = X X a A A X b B a := by create
+  _ = X X a A A B X a   := by move
+  _ = X A X A B X a     := by move
 
-lemma problem5 : A A X A = A A A X := calc
-  _ = A X a A A     := by move
+lemma problem9 : X a A A X = A X A X X := by calc
+  _ = X a A A X X := by create
+  _ = A X A X X   := by move
+
+lemma problem10 : A X a A A = A X A A X := by calc
   _ = X a A a A A   := by move
-  _ = X a a A A A   := by comm
-  _ = X a a a A A A := by problem3
-  _ = X a A a A a A := by comm
-  _ = A X a A a A   := by move
-  _ = A A X a A     := by move
-  _ = A A A X       := by move
+  _ = X a A A a A   := by comm
+  _ = X a A A X a A := by create
+  _ = A X A A X     := by move
 
-private lemma problem5' : x A A X A = x A A A X := by
-  simp only [← Tseitin.mul_assoc x, problem5]
+lemma problem11 : a b a A A B A A = a b a a b a A A B A A := by calc
+  _ = A A B a A A b a         := by comm
+  _ = A A B a A A X b a       := by create
+  _ = a A A B A A X b a       := by comm
+  _ = a A A X b B a A a A b a := by move
+  _ = a A A b B a A a A b a   := by create
+  _ = a b a a b a A A B A A   := by comm
 
-macro "problem5" : tactic => `(tactic| simp only [problem5, problem5'])
+lemma problem12 : a A A B A A = a b a a A A B A A := by calc
+  _ = A A B a A A         := by comm
+  _ = A A B a A A X       := by create
+  _ = a A A B A A X       := by comm
+  _ = a A A X b B a A a A := by move
+  _ = a A A b B a A a A   := by create
+  _ = a b a a A A B A A   := by comm
 
-lemma my_problem6 : A A X A = A X A A := calc
-  _ = A X a A A   := by move
-  _ = X a A a A A := by move
-  _ = X a a A A A := by comm
-  _ = X a A A A   := by problem3
-  _ = A X A A     := by move
+end beginner
 
-lemma example1 : A A X X = A A X := calc
-  _ = A X a A X   := by move
-  _ = X a A a A X := by move
-  _ = X a a A A X := by comm
-  _ = X a a A A   := by create
-  _ = X a A a A   := by comm
-  _ = A X a A     := by move
-  _ = A A X       := by move
+namespace intermediate
+
+lemma problem1 : A A A X = A A X A := by calc
+  _ = X a A a A a A := by move
+  _ = X a a a A A A := by comm
+  _ = X a a A A A   := by duplicate
+  _ = X a A a A A   := by comm
+  _ = A A X A       := by move
+
+lemma problem2 : a b a b A A A B A B A A = a b a b a b a b A A A B A B A A := by calc
+  _ = a b A A A B A B a A A b             := by comm
+  _ = a b A A A B A B a A A X b           := by create
+  _ = a b A a A A B A B A A X b           := by comm
+  _ = a b A a A A X b B a A b B a A a A b := by move
+  _ = a b A a A A b B a A b B a A a A b   := by create
+  _ = a b a b a b a a A A A b B A B A A   := by comm
+  _ = a b a b a b a A A A b B A B A A     := by duplicate
+  _ = a b a b a b a b A A A B A B A A     := by comm
+
+lemma problem3 : a a a a A A X = a a a A A X a := by calc
+  _ = a a a a A A   := by create
+  _ = a a a A A a   := by comm
+  _ = a a a A A X a := by create
+
+lemma problem4 : A X A A = A A X A := by calc
+  _ = X a A A A := by move
+  _ = X a a A A A := by duplicate
+  _ = X a A a A A := by comm
+  _ = A A X A := by move
+
+lemma problem5 : A X b a A A = A X b a a A A := by calc
+  _ = X a A b a A A := by move
+  _ = X a b a A A A := by comm
+  _ = X a b a a A A A := by duplicate
+  _ = X a A b a a A A := by comm
+  _ = A X b a a A A := by move
+
+lemma problem6 : a a a a A A X = a a A A X a a := by calc
+  _ = a a a a A A   := by create
+  _ = a a A A a a   := by comm
+  _ = a a A A X a a := by create
+
+-- lemma example1 : A A X X = A A X := calc
+--   _ = X a A a A X := by move
+--   _ = X a a A A X := by comm
+--   _ = X a a A A   := by create
+--   _ = X a A a A   := by comm
+--   _ = A A X       := by move
+
+end intermediate
 
 end positive
